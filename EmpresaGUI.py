f069b90d72a0be62032549b88ee80eb6215a4b1f
IssacTah <54335484+IssacTah@users.noreply.github.com>
2023-11-18 14:46:33 -0500

    Add files via upload

---
+++ EmpresaGUI.py
@@ -0,0 +1,117 @@
+import tkinter as tk
+import customtkinter
+import json
+import pandas as pd
+
+class EmpresaGUI(customtkinter.CTkFrame):
+    def __init__(self, parent, datos_empresa):
+        super().__init__(parent, corner_radius=0, fg_color="transparent")
+        self.grid_columnconfigure(0, weight=1)
+
+        # Etiquetas
+        label_datos_generales = customtkinter.CTkLabel(self, text="Datos Generales", font=customtkinter.CTkFont(size=30, weight="bold"),
+                                                       text_color="#0f4e9c")
+        labels = [
+            customtkinter.CTkLabel(self, text="Nombre", font=customtkinter.CTkFont(size=20)),
+            customtkinter.CTkLabel(self, text="RFC", font=customtkinter.CTkFont(size=20)),
+            customtkinter.CTkLabel(self, text="Dirección", font=customtkinter.CTkFont(size=20)),
+            customtkinter.CTkLabel(self, text="Regimen", font=customtkinter.CTkFont(size=20)),
+            customtkinter.CTkLabel(self, text="Ejercicio", font=customtkinter.CTkFont(size=20)),
+        ]
+
+        label_datos_generales = customtkinter.CTkLabel(self, text="Datos Generales", font=customtkinter.CTkFont(size=30, weight="bold"),
+                                                       text_color="#0f4e9c")
+        label_nombre = customtkinter.CTkLabel(self, text="Nombre", font=customtkinter.CTkFont(size=20))
+        label_rfc = customtkinter.CTkLabel(self, text="RFC", font=customtkinter.CTkFont(size=20))
+        label_direccion = customtkinter.CTkLabel(self, text="Dirección", font=customtkinter.CTkFont(size=20))
+        label_regimen = customtkinter.CTkLabel(self, text="Regimen", font=customtkinter.CTkFont(size=20))
+        label_ejercicio = customtkinter.CTkLabel(self, text="Ejercicio", font=customtkinter.CTkFont(size=20))
+
+        # Datos
+        data = pd.read_json("./datos/regimenes.json", orient="index")
+        data["Concatenado"] = data["Numero del regimen"] + " " + data["Nombre"]
+        values_concatenados = data["Concatenado"].tolist()
+
+        # Entradas y botones
+        self.nombre = customtkinter.CTkEntry(self, width=400, height=35, placeholder_text="Nombre de la empresa")
+        self.rfc = customtkinter.CTkEntry(self, width=400, height=35, placeholder_text="RFC de la empresa")
+        self.direccion = customtkinter.CTkEntry(self, width=400, height=35, placeholder_text="Dirección de la empresa")
+        self.regimen = customtkinter.CTkOptionMenu(self, width=400, height=35, values=values_concatenados)
+        self.ejercicio = customtkinter.CTkEntry(self, width=400, height=35, placeholder_text="Año")
+
+        # Botones adicionales
+        self.continuar = customtkinter.CTkButton(self, text="Continuar")
+        self.guardar = customtkinter.CTkButton(self, text="Guardar datos", command=self.guardar_datos)
+        self.editar_regimen = customtkinter.CTkButton(self, text="Editar Regimen", command=self.editar_regimenes)
+
+        label_datos_generales.place(relx=0.5, rely=0.1, anchor=tk.CENTER)
+        label_nombre.place(relx=0.35, rely=0.2, anchor=tk.CENTER)
+        label_rfc.place(relx=0.35, rely=0.3, anchor=tk.CENTER)
+        label_direccion.place(relx=0.35, rely=0.4, anchor=tk.CENTER)
+        label_regimen.place(relx=0.35, rely=0.5, anchor=tk.CENTER)
+        label_ejercicio.place(relx=0.35, rely=0.6, anchor=tk.CENTER)
+
+        entries = [self.nombre, self.rfc, self.direccion, self.regimen, self.ejercicio]
+        for i, entry in enumerate(entries):
+            entry.place(relx=0.6, rely=0.2 + i * 0.1, anchor=tk.CENTER)
+
+        buttons = [self.continuar, self.guardar, self.editar_regimen]
+        for i, button in enumerate(buttons):
+            button.place(relx=0.4 + i * 0.2, rely=0.8, anchor=tk.CENTER)
+
+        # Rellenar datos iniciales
+        self.nombre.insert(0, datos_empresa["nombre"])
+        self.rfc.insert(0, datos_empresa["rfc"])
+        self.direccion.insert(0, datos_empresa["direccion"])
+        self.ejercicio.insert(0, datos_empresa["ejercicio"])
+
+        # Establecer la referencia de la aplicación
+        self.set_app_reference(parent)
+
+    def set_app_reference(self, app):
+        self.app = app
+
+    def guardar_datos(self):
+        nombre_empresa = self.getterNombre()
+        rfc_empresa = self.getterRfc()
+        direccion_empresa = self.getterDireccion()
+        regimen_empresa = self.getterRegimen()
+        ejercicio_empresa = self.getterEjercicio()
+
+        save_data = SaveData()
+        data = GetData(nombre_empresa, rfc_empresa, direccion_empresa, regimen_empresa, ejercicio_empresa)
+        datos = data.getData()
+        save_data.saveData(datos)
+        self.app.guardar_datos()
+
+    def editar_regimenes(self):
+        import subprocess
+        subprocess.Popen(["python", "app/EditarRegimenGUI"])
+        self.app.editar_regimenes()
+
+
+
+class GetData:
+    def __init__(self, nombre, rfc, direccion, regimen, ejercicio):
+        self.nombre = nombre
+        self.rfc = rfc
+        self.direccion = direccion
+        self.regimen = regimen
+        self.ejercicio = ejercicio
+
+    def getData(self):
+        data = {
+            "nombre": self.nombre,
+            "rfc": self.rfc,
+            "direccion": self.direccion,
+            "regimen": self.regimen,
+            "ejercicio": self.ejercicio
+        }
+        return data
+
+
+class SaveData:
+    def saveData(self, data):
+        file_path = "datos/EmpresaDatos.json"
+        with open(file_path, "w") as json_file:
+            json.dump(data, json_file)
---
+++ Frame.py
@@ -0,0 +1,97 @@
+import customtkinter
+import os
+from PIL import Image
+import pandas as pd
+import json
+import tkinter as tk
+from EmpresaGUI import EmpresaGUI
+from SubirArchivosGUI import SubirArchivosGUI
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Pograma contable")
+        self.geometry("1100x450")
+
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(1, weight=1)
+
+        image_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_images")
+        self.logo_image = customtkinter.CTkImage(Image.open(os.path.join(image_path, "CustomTkinter_logo_single.png")), size=(26, 26))
+        self.home_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "home_dark.png")),
+                                                 dark_image=Image.open(os.path.join(image_path, "home_light.png")), size=(20, 20))
+        self.chat_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "chat_dark.png")),
+                                                 dark_image=Image.open(os.path.join(image_path, "chat_light.png")), size=(20, 20))
+        self.add_user_image = customtkinter.CTkImage(light_image=Image.open(os.path.join(image_path, "add_user_dark.png")),
+                                                     dark_image=Image.open(os.path.join(image_path, "add_user_light.png")), size=(20, 20))
+
+        self.navigation_frame = customtkinter.CTkFrame(self, corner_radius=0)
+        self.navigation_frame.grid(row=0, column=0, sticky="nsew")
+        self.navigation_frame.grid_rowconfigure(4, weight=1)
+
+        self.navigation_frame_label = customtkinter.CTkLabel(self.navigation_frame, text="  Programa Contable", image=self.logo_image,
+                                                             compound="left", font=customtkinter.CTkFont(size=15, weight="bold"))
+        self.navigation_frame_label.grid(row=0, column=0, padx=20, pady=20)
+
+        self.home_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Datos Empresa",
+                                                   fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                   image=self.home_image, anchor="w", command=self.home_button_event)
+        self.home_button.grid(row=1, column=0, sticky="ew")
+
+        self.frame_2_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Subir Archivos",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      image=self.chat_image, anchor="w", command=self.frame_2_button_event)
+        self.frame_2_button.grid(row=2, column=0, sticky="ew")
+
+        self.frame_3_button = customtkinter.CTkButton(self.navigation_frame, corner_radius=0, height=40, border_spacing=10, text="Verificar",
+                                                      fg_color="transparent", text_color=("gray10", "gray90"), hover_color=("gray70", "gray30"),
+                                                      image=self.add_user_image, anchor="w", command=self.frame_3_button_event)
+        self.frame_3_button.grid(row=3, column=0, sticky="ew")
+
+        self.appearance_mode_menu = customtkinter.CTkOptionMenu(self.navigation_frame, values=["Light", "Dark", "System"],
+                                                                command=self.change_appearance_mode_event)
+        self.appearance_mode_menu.grid(row=6, column=0, padx=20, pady=20, sticky="s")
+
+        with open('datos/EmpresaDatos.json', 'r') as archivo_json:
+            datos_empresa = json.load(archivo_json)
+
+        self.home_frame = EmpresaGUI(self, datos_empresa)
+        self.second_frame = SubirArchivosGUI(self)
+        self.third_frame = customtkinter.CTkFrame(self, corner_radius=0, fg_color="transparent")
+
+        self.select_frame_by_name("Datos Empresa")
+
+    def select_frame_by_name(self, name):
+        self.home_button.configure(fg_color=("gray75", "gray25") if name == "Datos Empresa" else "transparent")
+        self.frame_2_button.configure(fg_color=("gray75", "gray25") if name == "frame_2" else "transparent")
+        self.frame_3_button.configure(fg_color=("gray75", "gray25") if name == "frame_3" else "transparent")
+
+        if name == "Datos Empresa":
+            self.home_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.home_frame.grid_forget()
+        if name == "frame_2":
+            self.second_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.second_frame.grid_forget()
+        if name == "frame_3":
+            self.third_frame.grid(row=0, column=1, sticky="nsew")
+        else:
+            self.third_frame.grid_forget()
+
+    def home_button_event(self):
+        self.select_frame_by_name("Datos Empresa")
+
+    def frame_2_button_event(self):
+        self.select_frame_by_name("frame_2")
+
+    def frame_3_button_event(self):
+        self.select_frame_by_name("frame_3")
+
+    def change_appearance_mode_event(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
