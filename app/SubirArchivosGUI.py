9af9d4b9bf8f1c9b21593f1fb80d911457e220ab
santerd <tecnodiwaron@gmail.com>
2023-11-23 12:33:20 -0500

    Guardar xml en carpeta local

--- app/SubirArchivosGUI.py
+++ app/SubirArchivosGUI.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 import customtkinter
 import os
 import glob
+import shutil
 
 class SubirArchivosGUI(customtkinter.CTkFrame):
     def __init__(self, parent):
@@ -20,44 +21,40 @@ class SubirArchivosGUI(customtkinter.CTkFrame):
         
         # Agregar pestañas
         self.tabview.add("Subir Archivos XML")
-        self.tabview.add("Visualizar XML")
+        self.tabview.add("Mostrar contenido")
         
-        #Tabview 1
+        # Tabview 1
         self.string_input_button = customtkinter.CTkButton(self.tabview.tab("Subir Archivos XML"), text="Seleccionar Carpeta", command=self.uploadFile)
         self.string_input_button.pack(pady=20, padx=60, expand=True)
         
-        #Tabview 2
-        self.label = customtkinter.CTkLabel(self.tabview.tab("Visualizar XML"), text="Por favor, subir XML", font=customtkinter.CTkFont(size=20) )
+        # Tabview 2
+        self.label = customtkinter.CTkLabel(self.tabview.tab("Mostrar contenido"), text="Por favor, subir XML", font=customtkinter.CTkFont(size=20))
         self.label.pack(pady=20, padx=60, expand=True)
-        self.xml_listbox = Listbox(self.tabview.tab("Visualizar XML"))
+        self.xml_listbox = Listbox(self.tabview.tab("Mostrar contenido"))
     
     def set_app_reference(self, app):
         self.app = app
 
-    #Funcion para subir un archivo
+    # Función para subir un archivo
     def uploadFile(self):
         folder_path = filedialog.askdirectory()
         if folder_path:
+            # Crear una nueva carpeta en el directorio del programa para guardar los archivos XML
+            program_directory = os.path.dirname(os.path.realpath(__file__))
+            self.new_folder_path = os.path.join(program_directory, "XML")
+            os.makedirs(self.new_folder_path, exist_ok=True)
+
             xml_files = glob.glob(os.path.join(folder_path, '*.xml'))
             self.xml_listbox.delete(0, tkinter.END)
             for file in xml_files:
-                self.xml_listbox.insert(tkinter.END, os.path.basename(file))
+                # Copiar cada archivo XML a la nueva carpeta
+                new_file_path = shutil.copy(file, self.new_folder_path)
+                # Añadir el archivo al Listbox
+                self.xml_listbox.insert(tkinter.END, os.path.basename(new_file_path))
 
-            # Cambio aquí: Uso de pack en lugar de grid
             self.xml_listbox.pack(padx=20, pady=20, expand=True)
             self.label.pack_forget()
-            messagebox.showinfo("Carga de archivos completa", "Los archivos se ha cargado correctamente.")
+            messagebox.showinfo("Carga de archivos completa", "Los archivos se han cargado correctamente.")
         else:
             pass
-        self.app.uploadFile()
-    
-    #Analizando XML
-    def parse_xml(self, file_path):
-        try:
-            tree = ET.parse(file_path)
-            return tree.getroot()
-        except Exception as e:
-            messagebox.showerror("Error", f"No se pudo analizar el archivo XML:\n{str(e)}")
-            return None
-        finally:
-            self.app.parse_xml()
+        #self.app.uploadFile()
